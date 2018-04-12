963d4a30c64a7e88087db9483db357b4cab37b0b
ivanlmj <ivanlmj@gmail.com>
2018-04-11 21:00:49 -0500

    defining data model for book table

--- mybooks/books/models.py
+++ mybooks/books/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=200)
+    read_on = models.DateTimeField()
