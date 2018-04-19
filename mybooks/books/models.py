a2f09636fd2bd3fe69ada9151759f04a339009cc
ivanlmj <ivanlmj@gmail.com>
2018-04-18 23:43:58 -0500

    improving displayed information for book names

--- mybooks/books/models.py
+++ mybooks/books/models.py
@@ -4,3 +4,6 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     isbn = models.CharField(max_length=200)
     read_on = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
